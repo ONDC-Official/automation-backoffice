6fe89be61faa02908dc9e17ed00c290bf8c801ef
Sarthak Kavidayal <sarthak.kavidayal@thewitslab.com>
2025-01-13 15:53:41 +0530

    all keys changes

--- frontend/src/components/dashboard/index.tsx
+++ frontend/src/components/dashboard/index.tsx
@@ -18,6 +18,7 @@ const Dashboard = ({ label, db_id }: IProps) => {
   const navigate = useNavigate();
   const [isEdittable, setIsEdittable] = useState(false);
   const [editedPayload, setEditedPayload] = useState("");
+  const [allSession, setAllSession] = useState([]);
 
   const switchCacheDb = async () => {
     const userData = JSON.parse(
@@ -39,17 +40,46 @@ const Dashboard = ({ label, db_id }: IProps) => {
           },
         }
       );
+      getAllSession();
     } catch (e) {
       console.log("somthing went wrong while switching db", e);
       toast.error("Something went wrong");
     }
   };
 
+  const getAllSession = async () => {
+    const userData = JSON.parse(
+      localStorage.getItem("userData") || ("{}" as string)
+    );
+
+    if (!userData?.token) {
+      navigate("/");
+    }
+
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}/sessions/all`,
+        {
+          headers: {
+            Authorization: `Bearer ${userData.token}`,
+          },
+        }
+      );
+
+      console.log("Response data", response.data);
+      setAllSession(response.data);
+    } catch (e) {
+      console.log("somthing went wrong while fetching all  session", e);
+      toast.error("Something went wrong");
+    }
+  };
+
   useEffect(() => {
+    setCacheData(null);
     switchCacheDb();
   }, [db_id]);
 
-  const getCacheData = async () => {
+  const getCacheData = async (key: string) => {
     const userData = JSON.parse(localStorage.getItem("userData") as string);
 
     if (!userData?.token) {
@@ -60,7 +90,7 @@ const Dashboard = ({ label, db_id }: IProps) => {
       const response = await axios.get(
         `${import.meta.env.VITE_BACKEND_URL}/sessions`,
         {
-          params: { subscriber_url: subUrl },
+          params: { subscriber_url: key },
           headers: {
             Authorization: `Bearer ${userData.token}`,
           },
@@ -148,53 +178,80 @@ const Dashboard = ({ label, db_id }: IProps) => {
           placeholder="Subscriber Id"
           onChange={(data: string) => setSubUrl(data)}
         />
-        <Button label="Send" onClick={() => getCacheData()} />
+        <Button label="Send" onClick={() => getCacheData(subUrl)} />
       </div>
       <div>
-        <div className="p-2 flex flex-col gap-2">
-          {!cacheData && (
-            <p className="text-center">Search for cache using subscriber Id</p>
-          )}
-          {cacheData?.length === 0 && (
-            <p className="text-center">No Records found</p>
-          )}
-          {cacheData?.length > 0 && <h2>Results</h2>}
-          {cacheData?.length > 0 &&
-            cacheData.map((item: any) => {
+        {!cacheData ? (
+          <div>
+            <h2>Results</h2>
+            {allSession?.map((session: any) => {
               return (
-                <div className="h-72 flex flex-col relative">
-                  <div
-                    className={`absolute right-5 top-2 z-50 ${
-                      isEdittable ? "bg-blue-200" : "bg-gray-200"
-                    } p-2 rounded-md`}
-                    onClick={() => {
-                      setIsEdittable(!isEdittable);
-                      setEditedPayload(JSON.stringify(item, null, 2));
-                    }}
-                  >
-                    <MdEdit />
-                  </div>
-                  <Editor
-                    defaultLanguage="json"
-                    value={
-                      isEdittable
-                        ? editedPayload
-                        : JSON.stringify(item, null, 2)
-                    }
-                    onChange={(data: any) => setEditedPayload(data)}
-                    options={{
-                      minimap: { enabled: false },
-                      readOnly: !isEdittable,
-                    }}
-                  />
-                  <div className="w-full flex justify-end gap-2 p-2">
-                    <Button label="Delete" onClick={() => deleteCachedata()} />
-                    <Button label="Update" onClick={() => updateCachedata()} />
-                  </div>
+                <div
+                  className="border-b last:border-none px-4 py-3 hover:bg-gray-100 transition cursor-pointer"
+                  onClick={() => {
+                    setSubUrl(session);
+                    getCacheData(session);
+                  }}
+                >
+                  <span className="text-gray-700">{session}</span>
                 </div>
               );
             })}
-        </div>
+          </div>
+        ) : (
+          <div className="p-2 flex flex-col gap-2">
+            {!cacheData && (
+              <p className="text-center">
+                Search for cache using subscriber Id
+              </p>
+            )}
+            {cacheData?.length === 0 && (
+              <p className="text-center">No Records found</p>
+            )}
+            {cacheData?.length > 0 && <h2>Results</h2>}
+            {cacheData?.length > 0 &&
+              cacheData.map((item: any) => {
+                return (
+                  <div className="h-72 flex flex-col relative">
+                    <div
+                      className={`absolute right-5 top-2 z-50 ${
+                        isEdittable ? "bg-blue-200" : "bg-gray-200"
+                      } p-2 rounded-md`}
+                      onClick={() => {
+                        setIsEdittable(!isEdittable);
+                        setEditedPayload(JSON.stringify(item, null, 2));
+                      }}
+                    >
+                      <MdEdit />
+                    </div>
+                    <Editor
+                      defaultLanguage="json"
+                      value={
+                        isEdittable
+                          ? editedPayload
+                          : JSON.stringify(item, null, 2)
+                      }
+                      onChange={(data: any) => setEditedPayload(data)}
+                      options={{
+                        minimap: { enabled: false },
+                        readOnly: !isEdittable,
+                      }}
+                    />
+                    <div className="w-full flex justify-end gap-2 p-2">
+                      <Button
+                        label="Delete"
+                        onClick={() => deleteCachedata()}
+                      />
+                      <Button
+                        label="Update"
+                        onClick={() => updateCachedata()}
+                      />
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        )}
       </div>
     </div>
   );
